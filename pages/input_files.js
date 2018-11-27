69882f2f66757b8090a3b034c0692cd740d32ffa
Murilo Spinello <murilo.spinello@zup.com.br>
2018-11-26 22:09:10 -0200

    att

--- pages/input_files.js
+++ pages/input_files.js
@@ -3,11 +3,11 @@ let urlAuth = "https://keycloak-saas.apirealwave.io/auth/realms/sodexo/protocol/
 
 let urlFilesBase = "https://realwavecore-public-saas.gateway.zup.me/sodexoextension/v1"
 let urlXLSX = urlFilesBase + "/wizeo/upload?gw-app-key=9bb72ec0d3cf01364eeb000d3ac06d76"
-let urlJSON = urlFilesBase + "/beneficiofacil/upload?gw-app-key=9bb72ec0d3cf01364eeb000d3ac06d76"
-let urlTXT = urlFilesBase + "/pedefacil/upload?gw-app-key=9bb72ec0d3cf01364eeb000d3ac06d76"
+let urlTXT= urlFilesBase + "/beneficiofacil/upload?gw-app-key=9bb72ec0d3cf01364eeb000d3ac06d76"
+let urlJSON = urlFilesBase + "/pedefacil/upload?gw-app-key=9bb72ec0d3cf01364eeb000d3ac06d76"
 
 let username = "sodexo.poc.user"
-let password = "zup@2018" 
+let password = "zup@2018"
 
 function initAuth() {
 
@@ -18,9 +18,9 @@ function initAuth() {
 		(data) => {
 			if (data.access_token) {
 
-					initializeUploadInputs("#file1", urlXLSX, data.access_token);
-					initializeUploadInputs("#file2", urlTXT, data.access_token);
-					initializeUploadInputs("#file3", urlJSON, data.access_token);
+				initializeUploadInputs("file1", urlXLSX, data.access_token);
+				initializeUploadInputs("file2", urlTXT, data.access_token);
+				initializeUploadInputs("file3", urlJSON, data.access_token);
 			}
 		});
 }
@@ -60,6 +60,8 @@ $(document).ready(function () {
 		}
 
 		$(this).parent().find(".custom-file-label").text($(this).val())
+
+		
 	});
 
 	initAuth();
@@ -69,77 +71,94 @@ $(document).ready(function () {
 function initializeUploadInputs(idInput, url, access_token) {
 	//////
 	// Initialize the jQuery File Upload plugin
-	
+
 	let headers = {
 		"x-organization-slug": "sodexo",
 		"x-application-id": "sodexo_poc",
 		"Content-Type": "application/x-www-form-urlencoded",
-		"Authorization": `Bearer ${access_token}`,
-		"x-Application-key": "9bb72ec0d3cf01364eeb000d3ac06d76",
-		"filename": "test.json",
-		"origins": "*",
-		"Postman-Token": "55ee55b2-e24e-4032-8b64-5647d3dea2e5"
+		"Authorization": `Bearer ${access_token}`
+
 	};
 
-	$(function () {
-		$(idInput).fileupload({
-			url: url,
-			type: "POST",  
-			dataType: "json",
-			// forceIframeTransport:true,
-			"method": "POST", 
-			"x-organization-slug": "sodexo",
-			"x-application-id": "sodexo_poc",
-			"Content-Type": "application/x-www-form-urlencoded",
-			"Authorization": `Bearer ${access_token}`,
-			"x-Application-key": "9bb72ec0d3cf01364eeb000d3ac06d76",
-			"filename": "test.json",
-			"Postman-Token": "55ee55b2-e24e-4032-8b64-5647d3dea2e5",
-			headers: headers,
-			options: headers,
-			beforeSend: function(xhr) {
+	// Select your input type file and store it in a variable
+	const input = document.getElementById(idInput);
 
-				xhr.setRequestHeader("x-organization-slug", "sodexo")
-				xhr.setRequestHeader("x-application-id", "sodexo_poc")
-				xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-				xhr.setRequestHeader("Authorization", `Bearer ${access_token}`)
-				xhr.setRequestHeader("origins", "*")
-				xhr.setRequestHeader("x-Application-key", "9bb72ec0d3cf01364eeb000d3ac06d76")
-				xhr.setRequestHeader("filename", "test.json")		 
-				console.log("url", url)			
-				console.log("headers", headers)
+	// This will upload the file after having read it
+	const upload = (file) => {
+		var reader = new FileReader()
+		reader.onload = (e) => {
+ 
+			var arrayBuffer = reader.result;
 
-			}, 
-			send: function (e, data) {
+			fetch(url, { // Your POST endpoint
+				method: 'POST',
+				headers: headers,
+				body: arrayBuffer // This is your file object
+			}).then(
+				response => response.text() // if the response is a JSON object
+			).then(
+				success => console.log(success) // Handle the success response object
+			).catch(
+				error => console.log(error) // Handle the error response object
+			);
+		}
+		reader.readAsBinaryString(file);
+		
+	};
 
-				console.log("send", data)
-				// $.each(data.result.files, function (index, file) {
-				// 	$('<p/>').text(file.name).appendTo('#files');
-				// });
-			},
-			fail: function (e, data) {
+	// Event handler executed when a file is selected
+	const onSelectFile = () => upload(input.files[0]);
 
-				console.log("fail", data)
-				// $.each(data.result.files, function (index, file) {
-				// 	$('<p/>').text(file.name).appendTo('#files');
-				// });
-			},
-			done: function (e, data) {
+	// Add a listener on your input
+	// It will be triggered when a file will be selected
+	input.addEventListener('change', onSelectFile, false);
+ 
+		// $(idInput).fileupload({
+		// 	url: url,
+		// 	type: "POST",  
+		// 	dataType: "json",  
+		// 	headers: headers, 
+		// 	beforeSend: function(xhr) { 
+		// 		xhr.setRequestHeader("x-organization-slug", "sodexo")
+		// 		xhr.setRequestHeader("x-application-id", "sodexo_poc")
+		// 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+		// 		xhr.setRequestHeader("Authorization", `Bearer ${access_token}`)
+		// 		xhr.setRequestHeader("origins", "*")
+		// 		xhr.setRequestHeader("x-Application-key", "9bb72ec0d3cf01364eeb000d3ac06d76")
+		// 		xhr.setRequestHeader("filename", "test.json")		 
+		// 		console.log("url", url)			
+		// 		console.log("headers", headers)
 
-				console.log("done", data)
-				// $.each(data.result.files, function (index, file) {
-				// 	$('<p/>').text(file.name).appendTo('#files');
-				// });
-			},
-			progressall: function (e, data) {
-				var progress = parseInt(data.loaded / data.total * 100, 10);
-				console.log("progress", progress)
-				// $('#progress .progress-bar').css(
-				// 	'width',
-				// 	progress + '%'
-				// );
-			}
-		})
-	});
+		// 	}, 
+		// 	send: function (e, data) {
+
+		// 		console.log("send", data)
+		// 		// $.each(data.result.files, function (index, file) {
+		// 		// 	$('<p/>').text(file.name).appendTo('#files');
+		// 		// });
+		// 	},
+		// 	fail: function (e, data) {
+
+		// 		console.log("fail", data)
+		// 		// $.each(data.result.files, function (index, file) {
+		// 		// 	$('<p/>').text(file.name).appendTo('#files');
+		// 		// });
+		// 	},
+		// 	done: function (e, data) {
+
+		// 		console.log("done", data)
+		// 		// $.each(data.result.files, function (index, file) {
+		// 		// 	$('<p/>').text(file.name).appendTo('#files');
+		// 		// });
+		// 	},
+		// 	progressall: function (e, data) {
+		// 		var progress = parseInt(data.loaded / data.total * 100, 10);
+		// 		console.log("progress", progress)
+		// 		// $('#progress .progress-bar').css(
+		// 		// 	'width',
+		// 		// 	progress + '%'
+		// 		// );
+		// 	}
+		// }) 
 
 }
